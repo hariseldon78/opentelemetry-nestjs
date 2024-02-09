1faf9c7837ffaf31c7b2564a6f131666f064a16e
Roberto Previdi <hariseldon78@gmail.com>
2024-02-09 16:46:32 +0100

    fix backport

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,6 @@ export * from './OpenTelemetryModuleConfig';
 export * from './Trace/Decorators/Span';
 export * from './Trace/Decorators/Traceable';
 export * from './Trace/TraceService';
-export * from './Trace/TraceWrapper';
 export * from './Trace/Injectors/ControllerInjector';
 export * from './Trace/Injectors/EventEmitterInjector';
 export * from './Trace/Injectors/GuardInjector';
